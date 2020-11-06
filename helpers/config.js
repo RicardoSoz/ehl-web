cd2583316ef850493e5486cf2325a9a3424f9d49
RicardoSoz1 <artmindcoder@gmail.com>
2020-11-05 18:40:20 -0600

    working api

--- helpers/config.js
+++ helpers/config.js
@@ -1,5 +1,5 @@
 let config = {
-    host: 'us-cdbr-east-02.cleardb.com',
+    host: 'mysql://b5a8e7024c6cdd:4f5a046f@us-cdbr-east-02.cleardb.com/heroku_413cbc85ba7bd32?reconnect=true',
     user: 'b5a8e7024c6cdd',
     password: '4f5a046f',
     database: 'heroku_413cbc85ba7bd32'
